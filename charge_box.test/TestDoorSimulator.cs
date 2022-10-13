c0d1271cb3e9686ec5c2d0a8e4fd9d92a839d0d0
Ninjaneer127 <91457099+Ninjaneer127@users.noreply.github.com>
2022-10-13 13:11:24 +0200

    DoorSimulator,TestDoorSimulator done + fixed namespaces for UsbCharger part + uncommented stuff in Program

---
+++ charge_box.test/TestDoorSimulator.cs
@@ -0,0 +1,63 @@
+﻿using charge_box.classes;
+
+namespace charge_box.test;
+
+[TestFixture]
+public class TestDoorSimulator
+{
+    private DoorSimulator _uut;
+    private DoorOpenedEventArgs _receivedDoorOpenedEventArgs;
+    private DoorClosedEventArgs _receivedDoorClosedEventArgs;
+
+    [SetUp]
+    public void Setup()
+    {
+        _receivedDoorOpenedEventArgs = null;
+        _receivedDoorClosedEventArgs = null;
+
+        _uut = new DoorSimulator();
+
+        _uut.DoorOpenedEvent += (sender, args) =>
+        {
+            _receivedDoorOpenedEventArgs = args;
+        };
+       
+        _uut.DoorClosedEvent += (sender, args) =>
+        {
+            _receivedDoorClosedEventArgs = args;
+        };
+    }
+
+    [Test]
+    public void DoorUnlocked_DoorLocked_DoorOpened_NoEventFired()
+    {
+        _uut.UnlockDoor();
+        _uut.LockDoor();
+        _uut.OnDoorOpen();
+        Assert.That(_receivedDoorOpenedEventArgs, Is.Null);
+    }
+    [Test]
+    public void DoorUnlocked_DoorOpened_EventFired()
+    {
+        _uut.UnlockDoor();
+        _uut.OnDoorOpen();
+        Assert.That(_receivedDoorOpenedEventArgs, Is.Not.Null);
+    }
+    [Test]
+    public void DoorUnlocked_DoorOpened_DateTimeAsExpected()
+    {
+        var testTime = new DateTime(2022, 10, 13);
+        _uut.TestTime = testTime;
+        _uut.UnlockDoor();
+        _uut.OnDoorOpen();
+        Assert.That(_receivedDoorOpenedEventArgs.DoorOpenedTime, Is.EqualTo(testTime));
+    }
+    [Test]
+    public void DoorClosed_DateTimeAsExpected()
+    {
+        var testTime = new DateTime(2022, 10, 13);
+        _uut.TestTime = testTime;
+        _uut.OnDoorClose();
+        Assert.That(_receivedDoorClosedEventArgs.DoorClosedTime, Is.EqualTo(testTime));
+    }
+}
